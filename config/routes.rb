f5503017c672a4dd36602f6fe0b8f291bb064d17
Gaurav Arora <gauravarora.daiict@gmail.com>
2012-11-03 20:58:24 +0530

    Finish User signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 SampleApp::Application.routes.draw do
-  match '/signup', to: "User#new"
+  resources :users
+  match '/signup', to: "Users#new"
   root to: 'static_pages#home'
   match '/help', to: "static_pages#help"
   match '/about', to: "static_pages#about"
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -12,3 +12,37 @@ let(:base_text) { "Ruby on Rails Tutorial Sample App"}
 end
 end
 
+ describe "Profile Page" do
+  user = User.new(name: "Gaurav",email: "hkdj@kjdkl.com",password: "foobar",password_confirmation: "foobar");
+  user.save
+  user_new = User.find_by_email("hkdj@kjdkl.com") 
+  subject {page} 
+  before{visit user_path(user_new)}
+  it {should have_selector('h1',text: user.name)}
+  it {should have_selector('title',text: user.name)}
+ end
+
+  describe "signup" do
+    before {visit signup_path}
+    let(:submit) {"Create my account"}
+
+    describe "Just Clicking button without information" do
+      it "hould not create" do
+        expect {click_button submit}.not_to change(User,:count)
+       end
+    end
+
+    describe "with valid information" do
+        before do
+          fill_in "Name", with: "Mr. Arora"
+          fill_in "Email", with: "kushal18@gmail.com"
+          fill_in "Password", with: "foobar"
+          fill_in "Confirmation", with: "foobar"
+        end
+      it "Should create the user" do
+        expect {click_button submit}.to change(User,:count).by(1)
+      end
+    end
+
+  end
+
